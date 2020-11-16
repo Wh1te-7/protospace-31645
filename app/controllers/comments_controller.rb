de24b009315a7aa6986e562af4cd135858ca85cb
Wh1te-7 <ohas7cancer@icloud.com>
2020-11-16 20:04:50 +0900

    second

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,12 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      @coments = @prototype.comments
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index,:show]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index,:show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   def index
@@ -20,29 +21,26 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments
   end
 
   def edit 
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    if @prototype.save
-      redirect_to prototype_path
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
-      render :edit
+      rener :edit
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
-      render :index
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
@@ -51,13 +49,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
+
+
 end
